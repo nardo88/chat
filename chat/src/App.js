a1db1e54e64bc4ea92ddde9806577bbc7d096907
nardo88 <nardo1988@mail.ru>
2021-06-28 10:18:36 +0300

    1

--- chat/src/App.js
+++ chat/src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import firebase  from 'firebase/app';
+import 'firebase/database';
 
 function App() {
+
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyCu3CQAbFa26zynzviCn_ypxcTciKPGCiE",
+    authDomain: "chat-womanup.firebaseapp.com",
+    databaseURL: "https://chat-womanup-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "chat-womanup",
+    storageBucket: "chat-womanup.appspot.com",
+    messagingSenderId: "143420754973",
+    appId: "1:143420754973:web:0a58413071e07691208af4"
+  };
+
+  if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+}
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      чат
     </div>
   );
 }
